2a719961b4391025551c3110c73732f80a254990
Azerdox29 <lemattheo@gmail.com>
2024-01-13 01:59:07 +0100

    oui

--- app.js
+++ app.js
@@ -4,4 +4,52 @@ window.addEventListener('load', () => {
 
     loader.classList.add('fondu-out');
 
+});
+const toggleMenuOpen = () => document.body.classList.toggle("open");
+
+const active = document.querySelector('.active');
+const active1 = document.querySelector('.active1');
+const active2 = document.querySelector('.active2');
+const active3 = document.querySelector('.active3');
+
+active.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
 })
+
+active1.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 750,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
+
+active2.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 1550,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
+
+active3.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 2000,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
+
+
